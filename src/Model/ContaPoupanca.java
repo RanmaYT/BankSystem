887b795d1b39ba56749113ae51da6f7e2c7ffa4f
Jucyara F Santana <jucyaraferreira57@gmail.com>
2025-11-26 12:51:24 -0300

    Correção na conta poupança

--- src/Model/ContaPoupanca.java
+++ src/Model/ContaPoupanca.java
@@ -1,9 +1,19 @@
 package Model;
 
 import State.IContaState;
+import Exceptions.OperacaoNaoConcluidaException;
 
 public class ContaPoupanca extends ContaAbstrata{
     public ContaPoupanca(double saldo, String cpfTitular, IContaState state) {
         super(saldo, cpfTitular, state, "Poupança");
+
+    }
+    @Override
+    public void debitar(double valor) {
+        if(valor > getSaldo()) {
+            throw new OperacaoNaoConcluidaException("Valor insuficiente!");
+        }
+
+        super.debitar(valor);
     }
 }
